09eb604125117c1bd581885a608358e5a5de9dda
Pete Nawara <pete.nawara@21gramsny.com>
2020-02-13 12:42:54 -0500

    fix(global): remove branding

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,5 @@
 <svelte:head>
-	<title>HABIT66 | Change for good.</title>
+	<title>Title</title>
 </svelte:head>
 
 <!-- <svelte:window bind:innerHeight={sectionElement} /> -->
@@ -10,36 +10,99 @@
 </div>
 
 <section id="whatWeDo">
-	<h1>Some words speak louder than actions.</h1>
+	<h1>Theophrastus mediocriterne delectat, cum tractat locos ab Aristotele ante tractatos?</h1>
 
-	<p>Most of advertising tries to get people to feel some thing or think some way about a brand. But when it comes to health, it takes more than good juju and good intentions to move people.</p>
+	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Hoc non est positum in nostra actione. Qui est in parvis malis. Quippe: habes enim a rhetoribus; Quippe: habes enim a rhetoribus; Duo Reges: constructio interrete. <b>Hic ambiguo ludimur.</b> </p>
 
-	<p>We go one step further. We create healthcare communications designed to make people change how they act. And we help make those new behaviors so routine, people don’t even have to think about them.</p>
+	<ol>
+		<li>Quodcumque in mentem incideret, et quodcumque tamquam occurreret.</li>
+		<li>At eum nihili facit;</li>
+		<li>Sed vos squalidius, illorum vides quam niteat oratio.</li>
+		<li>Quid, si etiam iucunda memoria est praeteritorum malorum?</li>
+	</ol>
 
-	<p>Sound like mind control? It’s even cooler than that.</p>
+
+	<blockquote cite='http://loripsum.net'>
+		Quacumque enim ingredimur, in aliqua historia vestigium ponimus.
+	</blockquote>
+
+
+	<p>Sed in rebus apertissimis nimium longi sumus. Equidem e Cn. <i>Quid dubitas igitur mutare principia naturae?</i> Ergo hoc quidem apparet, nos ad agendum esse natos. </p>
+
+	<ul>
+		<li>Itaque mihi non satis videmini considerare quod iter sit naturae quaeque progressio.</li>
+		<li>Quis istud, quaeso, nesciebat?</li>
+		<li>Nos vero, inquit ille;</li>
+		<li>Sed ad illum redeo.</li>
+	</ul>
+
+
+
 </section>
 
 <section id="howWeDoIt">
-	<h1>We break it. Then fix it.</h1>
-	<p>Studies show that it takes, on average, 21 days to break a habit. And 66 days to make a new one. </p>
+	<h1>Ex rebus enim timiditas, non ex vocabulis nascitur.</h1>
 
-	<p>It’s not easy to shake people out of the behaviors that they do on autopilot. Unless you know what you’re doing. (Guess what?? We do.)</p>
-	<h2>
-		Smarter nerds
-	</h2>
-	<p>Our HABIT66 team is not your traditional advertising roster. We hire people with backgrounds in behavioral economics and behavioral psychology. People who know the latest evidence-based strategies to change not just the minds of your audience, but truly, their actions. This very specific set of skills is bolstered through our exclusive partnership with the Behavioral Economics team at <a href="https://www.cmu.edu/dietrich/behavioral-economics/index.html" target="_blank">Carnegie Mellon University</a>. </p>
-	<a href="https://www.cmu.edu/dietrich/behavioral-economics/index.html" target="_blank"><CmuLogo /></a>
-	<h2>Bigger data</h2>
-	<p>As part of W2O group, we leverage AOS, the best-in-class, propriety analytics platform. Want to know what a doctor prescribed last month? You probably know that already. But want to know what else she did that day? And why? And what she’ll prescribe next month? That’s AOS. We’ve mapped the digital footprint of 1.1 million health ecosystem profiles, including physicians, healthcare advocates, patients, and hospitals in 173 countries around the world. Boom.</p>
-	<a href="https://www.w2ogroup.com/analytics/" target="_blank"><W2oLogo /></a>
+	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suo genere perveniant ad extremum; Idem adhuc; Quodsi ipsam honestatem undique pertectam atque absolutam. Erit enim mecum, si tecum erit. Duo Reges: constructio interrete. </p>
+
+	<blockquote cite='http://loripsum.net'>
+		Quia voluptatem hanc esse sentiunt omnes, quam sensus accipiens movetur et iucunditate quadam perfunditur.
+	</blockquote>
+
+	<h2>Look at that</h2>
+
+
+	<ol>
+		<li>Nulla erit controversia.</li>
+		<li>Qui autem esse poteris, nisi te amor ipse ceperit?</li>
+		<li>Quod autem meum munus dicis non equidem recuso, sed te adiungo socium.</li>
+		<li>Non quam nostram quidem, inquit Pomponius iocans;</li>
+	</ol>
+
+
+	<p><a href='http://loripsum.net/' target='_blank'>Hoc etsi multimodis reprehendi potest, tamen accipio, quod dant.</a> Falli igitur possumus. Sed virtutem ipsam inchoavit, nihil amplius. Addidisti ad extremum etiam indoctum fuisse. <a href='http://loripsum.net/' target='_blank'>Respondent extrema primis, media utrisque, omnia omnibus.</a> Si verbum sequimur, primum longius verbum praepositum quam bonum. </p>
+
+	<ul>
+		<li>Aufidio, praetorio, erudito homine, oculis capto, saepe audiebam, cum se lucis magis quam utilitatis desiderio moveri diceret.</li>
+		<li>Quacumque enim ingredimur, in aliqua historia vestigium ponimus.</li>
+		<li>Haec bene dicuntur, nec ego repugno, sed inter sese ipsa pugnant.</li>
+		<li>Ut pulsi recurrant?</li>
+	</ul>
+
+
+
 </section>
 
 <section id="contact">
-	<h1>Expectations, like habits, were meant to be broken. </h1>
-	<p>You give us the behavior you want to change. We’ll give you a plan for the first 66-days. </p>
-	<p>
-	Contact us at <a href="mailto:hello@habit66.com">hello@habit66.com</a>
-	</p>
+	<h1>Negat esse eam, inquit, propter se expetendam.</h1>
+
+	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. <a href='http://loripsum.net/' target='_blank'>Itaque contra est, ac dicitis;</a> Sed vos squalidius, illorum vides quam niteat oratio. Negare non possum. Duo Reges: constructio interrete. </p>
+
+	<ul>
+		<li>At ego quem huic anteponam non audeo dicere;</li>
+		<li>Paulum, cum regem Persem captum adduceret, eodem flumine invectio?</li>
+		<li>Ita relinquet duas, de quibus etiam atque etiam consideret.</li>
+		<li>Quid ergo aliud intellegetur nisi uti ne quae pars naturae neglegatur?</li>
+		<li>Familiares nostros, credo, Sironem dicis et Philodemum, cum optimos viros, tum homines doctissimos.</li>
+		<li>Qua tu etiam inprudens utebare non numquam.</li>
+	</ul>
+
+
+	<blockquote cite='http://loripsum.net'>
+		Octavio fuit, cum illam severitatem in eo filio adhibuit, quem in adoptionem D.
+	</blockquote>
+
+
+	<ol>
+		<li>Quae qui non vident, nihil umquam magnum ac cognitione dignum amaverunt.</li>
+		<li>Modo etiam paulum ad dexteram de via declinavi, ut ad Pericli sepulcrum accederem.</li>
+		<li>Pisone in eo gymnasio, quod Ptolomaeum vocatur, unaque nobiscum Q.</li>
+	</ol>
+
+
+	<p>An eiusdem modi? <a href='http://loripsum.net/' target='_blank'>Restinguet citius, si ardentem acceperit.</a> Itaque hic ipse iam pridem est reiectus; <a href='http://loripsum.net/' target='_blank'>Philosophi autem in suis lectulis plerumque moriuntur.</a> Sed quot homines, tot sententiae; <b>Si longus, levis;</b> </p>
+
+
 </section>
 
 
